d6d701d1ff6e1c66035e06d42ab684e3220e1406
gmisek <gmisek@gmail.com>
2013-02-17 16:47:44 -0500

    enhance admin section

--- web/discussthings/admin.py
+++ web/discussthings/admin.py
@@ -3,6 +3,25 @@ from models import Thing
 from models import Topic
 from models import Response
 
-admin.site.register(Thing)
-admin.site.register(Topic)
-admin.site.register(Response)
+
+class TopicInline(admin.TabularInline):
+    model = Topic
+
+
+class ResponseInline(admin.TabularInline):
+    model = Response
+    list_display = ('date_created', 'author', 'body')
+
+
+class TopicAdmin(admin.ModelAdmin):
+    inlines = [ResponseInline]
+    list_display = ('date_created', 'author', 'body')
+
+
+class ThingAdmin(admin.ModelAdmin):
+    inlines = [TopicInline]
+
+
+admin.site.register(Thing, ThingAdmin)
+admin.site.register(Topic, TopicAdmin)
+#admin.site.register(Response)
